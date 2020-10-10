a92a5bbfc8f9897d573fb00f5f03e5ea65380a49
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-10-10 15:32:30 +0100

    Added skeleton function for text mode print in case ncurses is not available

--- knowledgerep/A1_COMP9016_Bhattacharjee_Rajbir_R00195734.py
+++ knowledgerep/A1_COMP9016_Bhattacharjee_Rajbir_R00195734.py
@@ -1,6 +1,10 @@
 import os,sys,inspect
 import time
 
+# Is curses available or not?
+g_curses_available = True
+
+# Import the AIMA libraries from the parent directory
 try:
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     parentdir = os.path.dirname(currentdir)
@@ -10,11 +14,12 @@ except:
     print("Could not import from parent folder... Exiting")
     sys.exit(1)
 
+# Curses is only available in linux, in windows, continue without it
 try:
     import curses
 except:
-    print("Could not import ncurses. Please install ncurses... Exiting")
-    sys.exit(1)
+    g_curses_available = False
+    print("Curses is not available, continuing without it...")
 
 class TwoDThing(Thing):
     def __init__(self, x = 0, y = 0, display='T'):
@@ -23,29 +28,42 @@ class TwoDThing(Thing):
         self.y = y
         self.display = display
 
-    def get_location():
+    def get_location(self):
         return (self.x, self.y)
 
-    def get_display():
+    def get_display(self):
         return self.display
 
 
+class Wall(TwoDThing):
+    def __init__(self, x, y):
+        super().__init__(x, y, '#')
 
-class Wall(Thing):
-    pass
+class Door(TwoDThing):
+    def __init__(self, x, y):
+        super().__init__(x, y, 'D')
 
 # Create our own 2D environment
 class TwoDEnvironment(Environment):
     def __init__(self, rows, cols):
+        global g_curses_available
         super().__init__()
         self.rows = rows
         self.cols = cols
-        self.window = curses.initscr()
+        if g_curses_available:
+            self.window = curses.initscr()
     
     def __del__(self):
-        curses.endwin()
+        global g_curses_available
+        if g_curses_available:
+            curses.endwin()
 
-    def print_state(self):
+    # If curses is not supported, print the maze in text format
+    def print_state_text(self):
+        print("TODO: code print_state_text")
+
+    # Use ncurses to print the maze
+    def print_state_curses(self):
         for i in range(self.cols + 2):
             self.window.addstr(0, i, '#')
             self.window.addstr(self.rows+1, i, '#')
@@ -54,8 +72,20 @@ class TwoDEnvironment(Environment):
             self.window.addstr(i+1, self.cols + 1, '#')
         for i in range(self.rows):
             for j in range(self.cols):
-                self.window.addstr(i+1, j+1, 'E')
+                self.window.addstr(i+1, j+1, ' ')
+        for thing in self.things:
+            if isinstance(thing, TwoDThing):
+                x, y = thing.get_location()
+                c = thing.get_display()
+                self.window.addstr(x+1, y+1, c)
         self.window.refresh()
+
+    def print_state(self):
+        global g_curses_available
+        if g_curses_available:
+            return self.print_state_curses()
+        else:
+            return self.print_state_text()
 
 class TwoDMaze(TwoDEnvironment):
     def __init__(self, mazeString=str):
@@ -63,6 +93,14 @@ class TwoDMaze(TwoDEnvironment):
         rows = len(mazeString)
         cols = len(mazeString[0])
         super().__init__(rows, cols)
+        for i in range(rows):
+            for j in range(cols):
+                if '#' == mazeString[i][j]:
+                    wall = Wall(i, j)
+                    self.add_thing(wall)
+                if 'x' == mazeString[i][j]:
+                    door = Door(i, j)
+                    self.add_thing(door)
 
 
 def create_2d_environment(maze=str):
