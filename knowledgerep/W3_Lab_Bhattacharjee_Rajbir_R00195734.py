d3da78231183b5894eb2c6bfeb3ce5013f0f363f
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-10-13 00:35:14 +0100

    Removed python warning

--- knowledgerep/W3_Lab_Bhattacharjee_Rajbir_R00195734.py
+++ knowledgerep/W3_Lab_Bhattacharjee_Rajbir_R00195734.py
@@ -91,6 +91,7 @@ class MissionariesProblem(Problem):
     def value(self, state):
         b2 = state[3] + state[4] + state[5]
         b1 = state[0] + state[1] + state[2]
+        return b
 
 def my_heuristic(state):
     s = state.state
