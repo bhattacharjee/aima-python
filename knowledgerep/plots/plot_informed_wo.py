1d5834f031fa2335ac4306b17213423ca8741c4f
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-10-25 01:49:02 +0100

    Some more plots

---
+++ knowledgerep/plots/plot_informed_wo.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+
+fig, ax = plt.subplots(2, 2)
+print(ax, type(ax), len(ax))
+
+x = [10.09950494, 16.43167673, 27.03701167, 76.15773106 ]
+
+ax[0][0].set(title="nSteps", xlabel="sqrt(n_squares)", ylabel="nSteps")
+y1 = [15, 36, 77]
+ax[0][0].plot(x[:3], y1, label="GRBFS w/o Length", color="blue", marker='o')
+y1 = [2.05, 3.655, 3.59, 547.19]
+ax[0][0].plot(x, y1, label="A* w/o Length", color="green", marker='o')
+y1 = [15]
+ax[0][0].plot(x[:1], y1, label="RBFS w/o Length", color="red", marker='^')
+ax[0][0].legend()
+fig.legend()
+
+ax[0][1].set(title="Successors", xlabel="sqrt(n_squares)", ylabel="Successors")
+y1 = [23, 39, 137, ]
+ax[0][1].plot(x[:3], y1, label="GRBFS w/o Length", color="blue", marker='o')
+y1 = [28, 588, 198, 12305, ]
+ax[0][1].plot(x, y1, label="A* w/o Length", color="green", marker='o')
+y1 = [35]
+ax[0][1].plot(x[:1], y1, label="RBFS w/o Length", color="red", marker='^')
+
+
+ax[1][0].set(title="Search Time", xlabel="sqrt(n_squares)", ylabel="Search Time (s)")
+y1 = [0.088, 0.293, 2.053, ]
+ax[1][0].plot(x[:3], y1, label="GRBFS w/o Length", color="blue", marker='o')
+y1 = [0.085, 3.346, 3.213, 1100.5, ]
+ax[1][0].plot(x, y1, label="A* w/o Length", color="green", marker='o')
+y1 = [0.112]
+ax[1][0].plot(x[:1], y1, label="RBFS w/o Length", color="red", marker='^')
+
+ax[1][1].set(title="Memory", xlabel="sqrt(n_squares)", ylabel="MB")
+y1 = [66.17, 66.51, 66.92, ]
+ax[1][1].plot(x[:3], y1, label="GRBFS w/o Length", color="blue", marker='o')
+y1 = [66.47, 67.41, 67.11, 278.68, ]
+ax[1][1].plot(x, y1, label="A* w/o Length", color="green", marker='o')
+y1 = [66.4]
+ax[1][1].plot(x[:1], y1, label="RBFS w/o Length", color="red", marker='^')
+
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25, wspace=0.35)
+plt.show()
---
+++ knowledgerep/plots/plot_uninformed_hist.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+
+fig, ax = plt.subplots(2, 2)
+print(ax, type(ax), len(ax))
+
+x = [10.09950494, 16.43167673, 27.03701167]
+x1 = [10.09950494, 27.03701167]
+
+ax[0][0].set(title="nSteps", xlabel="sqrt(n_squares)", ylabel="nSteps")
+y1 = [33, 110, 327]
+ax[0][0].plot(x, y1, label="DFS with Length", color="blue", marker='o')
+y1 = [15, 36, 53]
+ax[0][0].plot(x, y1, label="BFS with Length", color="red", marker='o')
+y1 = [15, 53, ]
+ax[0][0].plot(x1, y1, label="UCS with Length", color="green", marker='o')
+ax[0][0].set_ylim(bottom=0)
+ax[0][0].legend()
+fig.legend()
+
+ax[0][1].set(title="Successors", xlabel="sqrt(n_squares)", ylabel="Successors")
+y1 = [45, 115, 404, ]
+ax[0][1].plot(x, y1, label="DFS with Length", color="blue", marker='o')
+y1 = [283, 219557, 227716, ]
+ax[0][1].plot(x, y1, label="BFS with Length", color="red", marker='o')
+y1 = [377, 231030]
+ax[0][1].plot(x1, y1, label="UCS with Length", color="green", marker='o')
+
+
+ax[1][0].set(title="Search Time", xlabel="sqrt(n_squares)", ylabel="Search Time (s)")
+y1 = [0.064, 0.413, 2.116, ]
+ax[1][0].plot(x, y1, label="DFS with Length", color="blue", marker='o')
+y1 = [0.276, 2584.57, 2691.58, ]
+ax[1][0].plot(x, y1, label="BFS with Length", color="red", marker='o')
+y1 = [0.3912, 4681.72812]
+ax[1][0].plot(x1, y1, label="UCS with Length", color="green", marker='o')
+
+ax[1][1].set(title="Memory", xlabel="sqrt(n_squares)", ylabel="MB")
+y1 = [66.48, 66.41, 67.6, ]
+ax[1][1].plot(x, y1, label="DFS with Length", color="blue", marker='o')
+y1 = [66.45, 309.58, 471.43, ]
+ax[1][1].plot(x, y1, label="BFS with Length", color="red", marker='o')
+y1 = [66.59, 476.1796875]
+ax[1][1].plot(x1, y1, label="UCS with Length", color="green", marker='o')
+
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+#plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25, wspace=0.35)
+plt.show()
