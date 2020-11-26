04c77ef14368377289393084a63e94397c3b8281
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-11-26 20:39:09 +0000

    Adding some data

--- knowledgerep/A2_COMP9016_Bhattacharjee_Rajbir_R00195734.py
+++ knowledgerep/A2_COMP9016_Bhattacharjee_Rajbir_R00195734.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 
-print("Hello world")
+class NaiveBayesTextClassifier(object):
+
+    def __init__(self):
+        self.classes = set()
+        self.vocabulary = set()
+        self.records = list()
+
+        pass
+
+    def add_document(self, doc:str, doc_class:str)->None:
+        assert(isinstance(doc_class, str) and isinstance(doc, str))
+
+
+
