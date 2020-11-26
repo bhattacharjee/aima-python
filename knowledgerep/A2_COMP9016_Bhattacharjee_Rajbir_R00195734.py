2e8ba4b7ff1f45d9e899f578aeb1e1c9261731c4
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-11-26 23:27:48 +0000

    Naive bayes now fully works

--- knowledgerep/A2_COMP9016_Bhattacharjee_Rajbir_R00195734.py
+++ knowledgerep/A2_COMP9016_Bhattacharjee_Rajbir_R00195734.py
@@ -4,6 +4,8 @@ import re
 import string
 import pandas as pd
 import random
+import json
+import math
 random.seed(12345)
 
 class NaiveBayesTextClassifier(object):
@@ -12,22 +14,23 @@ class NaiveBayesTextClassifier(object):
         # the set of all classes
         self.classes = set()
 
-        # the vocabullary
+        # the vocabullary, all words irrespective of class
         self.vocabulary = set()
 
-        self.records = list()
-
-        # count of each word in each class
-        self.word_count_by_class = dict()
+        # for each class c, count of all the words in that class, count(w, c)
+        self.count_w_c = dict()
 
         # the full training data
         self.train_data = None
 
-        # prior probabilities for each class
+        # prior probabilities for each class log(p(c))
         self.priors = dict()
 
-        # Total number of words in all documents of each class
-        self.total_word_count_by_class = dict()
+        # conditional probabilities log(P(w|c))
+        self.p_w_c = dict()
+
+        # For each class w, count of all words in that class, count(c)
+        self.count_c = dict()
         pass
 
     def cleanup_line(self, line:str)->list:
@@ -45,6 +48,7 @@ class NaiveBayesTextClassifier(object):
         if len(ret) < 0:
             raise AssertionError("cleaned up line contains no characters")
         last_c = None
+        # If a number and a non-number are joined together, put a space in between
         for c in ret:
             if last_c != None:
                 if last_c in list('0123456789') and c not in list('0123456789'):
@@ -53,7 +57,10 @@ class NaiveBayesTextClassifier(object):
                     ret2.append(' ')
             last_c = c
             ret2.append(c)
-        return "".join(ret2)
+        ret = "".join(ret2)
+        # Replace numbers with ###
+        ret = re.sub(r'\d+', r'###', ret)
+        return ret
 
     def get_words(self, line:str)->list:
         """
@@ -76,10 +83,63 @@ class NaiveBayesTextClassifier(object):
             rows = self.train_data.loc[self.train_data['class_label'] == c]
             n_c = len(rows)
             # TODO: Apply smoothing here
-            self.priors[c] = n_c / n_inst
+            self.priors[c] = math.log(n_c / n_inst)
 
     def update_document(self, class_label:str, document:str)->None:
-        print(class_label, self.get_words(document))
+        if class_label not in self.count_w_c.keys():
+            self.count_w_c.keys[class_label] = dict()
+        if class_label not in self.count_c.keys():
+            self.count_c[class_label] = 0
+        for word in self.get_words(document):
+            self.count_c[class_label] += 1
+            if word not in self.count_w_c[class_label].keys():
+                self.count_w_c[class_label][word] = 0
+            self.count_w_c[class_label][word] += 1
+        if word not in self.vocabulary:
+            self.vocabulary.add(word)
+
+    def calculate_conditionals(self):
+        for word in self.vocabulary:
+            self.p_w_c[word] = {}
+            for c in self.classes:
+                c_w_c = 0
+                c_c = 0
+                try:
+                    c_w_c = self.count_w_c[c][word]
+                    c_c = self.count_c[c]
+                except:
+                    pass
+                    # TODO: Print a message on debug
+                pwc = (c_w_c + 1) / (c_c + len(self.vocabulary))
+                assert(pwc >= 0 and pwc <= 1)
+                pwc = math.log(pwc)
+                self.p_w_c[word][c] = pwc
+
+    def predict_one(self, document:str)->str:
+        all_ps = []
+        all_class_p_tuples = []
+        for c in self.classes:
+            p = self.priors[c]
+            for word in self.get_words(document):
+                try:
+                    p += self.p_w_c[word][c]
+                except:
+                    pass
+            print(c, p)
+            all_ps.append(p)
+            all_class_p_tuples.append((c, p, ))
+        max_p = max(all_ps)
+        for c, p in all_class_p_tuples:
+            if p == max_p:
+                return c
+
+    def predict(self, documents:list)->list:
+        ys = []
+        for document in documents:
+            y = self.predict_one(document)
+            ys.append(y)
+        return ys
+
 
     def fit(self, df:pd.DataFrame)->None:
         # Save the training data
@@ -88,8 +148,8 @@ class NaiveBayesTextClassifier(object):
         # First get the classes
         self.classes = set(df.class_label.unique())
         for this_class in self.classes:
-            self.word_count_by_class[this_class] = {}
-            self.total_word_count_by_class[this_class] = 0
+            self.count_w_c[this_class] = {}
+            self.count_c[this_class] = 0
 
         # Calculate priori probabilities of all classes
         self.calculate_priors()
@@ -98,11 +158,11 @@ class NaiveBayesTextClassifier(object):
             class_label = df.iloc[i].class_label
             document = df.iloc[i].document
             self.update_document(class_label, document)
+
+        self.calculate_conditionals()
         # Identify all unique words
         # Calculate Priori Probabilities
         # Calculate conditional probabilities of each word
-
-
 
 
 def read_lines(filename:str)->list:
@@ -161,10 +221,23 @@ def read_lines_and_convert_to_df(filename)->pd.DataFrame:
     df = pd.DataFrame({'class_label': labels, 'document': documents})
     return df
 
+def get_accuracy_score(y_test, y_predict):
+    assert(len(y_test) == len(y_predict))
+    n = len(y_test)
+    c = 0
+    for i, j in zip(y_test, y_predict):
+        if i == j:
+            c += 1
+    return c/n
+
 def main():
-    df = read_lines_and_convert_to_df("small.txt")
+    df = read_lines_and_convert_to_df('SMSSpamCollection')
     nb = NaiveBayesTextClassifier()
     train, test = train_test_split(df)
     nb.fit(train)
+    X_test = test.document.to_list()
+    y_test = test.class_label.to_list()
+    y_predict = nb.predict(X_test)
+    print(get_accuracy_score(y_predict, y_test))
 
 main()
